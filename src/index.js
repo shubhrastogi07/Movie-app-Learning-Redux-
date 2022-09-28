b2b4610d031fe33f9e5781b2912c98c6cbcf46e0
Shubh Rastogi <shubhrastogi777@gmail.com>
2022-09-28 12:06:07 +0530

    Adding Action, reducers, store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
+import { legacy_createStore as createStore } from "redux";
+import movies from "./reducers";
+
+const store = createStore(movies);
+console.log("store", store);
+console.log("Before STATE", store.getState());
+store.dispatch({
+  type: "ADD_MOVIES",
+  movies: [{ name: "Superman " }],
+});
+console.log("After State", store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
